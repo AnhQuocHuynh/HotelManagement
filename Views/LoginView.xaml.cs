f06a0beda912b1a4a928824f850a6a7ca45cd637
AnhQuocHuynh <quocpro12334@gmail.com>
2025-05-06 00:20:22 +0700

    commit 2

--- Views/LoginView.xaml.cs
+++ Views/LoginView.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,20 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using HotelManager.ViewModels;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 namespace HotelManager.Views
 {
-    public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
     {
-        public MainWindow()
+        public LoginView()
         {
             InitializeComponent();
-            DataContext = new MainViewModel();
         }
     }
+    
 }
