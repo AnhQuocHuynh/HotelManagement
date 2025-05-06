7827eab985e1a2b7f774fb60bb020765bc6f7b53
AnhQuocHuynh <quocpro12334@gmail.com>
2025-05-06 21:42:34 +0700

    Update PaymentView.xaml.cs

--- Views/PaymentView.xaml.cs
+++ Views/PaymentView.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace HotelManager.Views
+namespace HotelManager.Views.StaffViews
 {
     /// <summary>
     /// Interaction logic for PaymentView.xaml
