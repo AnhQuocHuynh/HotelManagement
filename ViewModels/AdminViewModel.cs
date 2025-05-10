8739b1ccb5f4cff43eb1a22348a5eb04ab1f99b7
ViciousEve <tuannguyenonair@gmail.com>
2025-05-10 12:45:58 +0700

    setup 1 số thứ

--- ViewModels/AdminViewModel.cs
+++ ViewModels/AdminViewModel.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HotelManager.ViewModels
 {
-    internal class AdminViewModel
+    //Tuấn
+    //Todo: Show room state, income, change properties {number of rooms, price, other fees}
+    internal class AdminViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        // Add properties and methods for the AdminViewModel here
+
     }
 }
