91ee009ddfb21be56cc8863aac26dd01bdac3e07
baobui13 <23520093@gm.uit.edu.vn>
2025-06-09 17:35:50 +0700

    OnConfiguring for sql on visual studio

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,24 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using HotelManager.Config;
+using HotelManager.Data;
+using HotelManager.Views;
+using Microsoft.EntityFrameworkCore;
 using System.Windows;
 
-namespace HotelManager;
-
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
-public partial class App : Application
+namespace HotelManager
 {
-}
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
 
+            var optionsBuilder = new DbContextOptionsBuilder<HotelDbContext>();
+            optionsBuilder.UseSqlServer(DatabaseConfig.GetConnectionString());
+
+            using (var context = new HotelDbContext(optionsBuilder.Options))
+            {
+                HotelDbInitializer.Seed(context);
+            }
+        }
+    }
+}
