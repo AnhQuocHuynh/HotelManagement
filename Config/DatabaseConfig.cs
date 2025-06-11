8a0f44c073ec7bb1aa817037545bc2a0e1a31634
baobui13 <23520093@gm.uit.edu.vn>
2025-06-12 02:09:46 +0700

    db config

---
+++ Config/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace HotelManager.Config
+{
+    public static class DatabaseConfig
+    {
+        public const string ServerName = "(localdb)\\ProjectModels"; //Điền ServerName trong SQL Server của mình thay cho "LAPTOP-CUA-QUOC\\SQLEXPRESS01"
+        public const string DatabaseName = "HotelManager";
+        public const bool IntegratedSecurity = true;
+
+        public static string GetConnectionString()
+        {
+            return $"Server={ServerName};Database={DatabaseName};Trusted_Connection=True;TrustServerCertificate=True;";
+        }
+    }
+} 
