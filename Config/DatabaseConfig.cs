99eb1816417fc03dc0549db7462657639e9ba7e4
ViciousEve <tuannguyenonair@gmail.com>
2025-06-18 09:33:22 +0700

    Update DBconfig + gitignore

---
+++ Config/DatabaseConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace HotelManager.Config
+{
+    public static class DatabaseConfig
+    {
+        public const string ServerName = @"(local)\SQLEXPRESS";
+        public const string DatabaseName = "HotelManager";
+        public const bool IntegratedSecurity = true;
+
+        public static string GetConnectionString()
+        {
+            return $"Server={ServerName};Database={DatabaseName};Trusted_Connection={IntegratedSecurity};TrustServerCertificate=True;";
+        }
+    }
+}
