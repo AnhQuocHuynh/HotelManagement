13dc87ff54a2e48f2931e05191de8588a48d262f
AnhQuocHuynh <quocpro12334@gmail.com>
2025-07-07 02:02:21 +0700

    Fix merge conflicts: restore corrupted files from backups and resolve conflict markers

--- HotelManager.Core/Config/DatabaseConfig.cs
+++ HotelManager.Core/Config/DatabaseConfig.cs
@@ -7,7 +7,7 @@ namespace HotelManager.Config
     public static class DatabaseConfig
     {
         // Support multiple server configurations - team members can change as needed
-        public const string ServerName = @"(localdb)\ProjectModels"; // Default for BuiQuocBao, change to "(local)\SQLEXPRESS" for Quốc setup
+        public const string ServerName = @"(local)\SQLEXPRESS"; // Default for BuiQuocBao, change to "(local)\SQLEXPRESS" for Quốc setup
         public const string DatabaseName = "HotelManager";
         public const bool IntegratedSecurity = true;
 
