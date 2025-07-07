fb83d6ef9df4ad8c546d0340eb144892fed0f1db
baobui13 <23520093@gm.uit.edu.vn>
2025-07-07 13:15:42 +0700

    re-config

--- Config/DatabaseConfig.cs
+++ Config/DatabaseConfig.cs
@@ -2,7 +2,7 @@ namespace HotelManager.Core.Config
 {
     public static class DatabaseConfig
     {
-        public const string ServerName = "LAPTOP-CUA-QUOC\\SQLEXPRESS";
+        public const string ServerName = @"(localdb)\ProjectModels";
         public const string DatabaseName = "HotelManager";
         public const bool IntegratedSecurity = true;
 
