5f6fe23982511a0685e6f547292d03f44c6d05ba
baobui13 <23520093@gm.uit.edu.vn>
2025-07-11 14:16:42 +0700

    Update xml layout

---
+++ Views/StaffViews/MyScheduleView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HotelManager.Views.StaffViews
+{
+    /// <summary>
+    /// Interaction logic for MyScheduleView.xaml
+    /// </summary>
+    public partial class MyScheduleView : UserControl
+    {
+        public MyScheduleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
